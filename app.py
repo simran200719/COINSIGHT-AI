a56dfd57bd9f14e1ed2a27949d2048bdec4682bd
gursimranjot kaur <simran19@users.noreply.huggingface.co>
2025-09-20 16:56:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ def home():
 
 @app.route('/predict',methods=['POST','GET'])
 def pred():
-    Open=float(scaler.fit_tranform(request.form.get('open'))
-    High=float(scaler.fit_transform(request.form.get('high'))
-    Low=float(scaler.fit_transform(request.form.get('low'))
-    Close=float(sclaer.fit_transform(request.form.get('close'))	
-    Volume=float(scaler.fit_transform(request.form.get('volume'))
+    Open=float(scaler.fit_tranform(request.form.get('open')))
+    High=float(scaler.fit_transform(request.form.get('high')))
+    Low=float(scaler.fit_transform(request.form.get('low')))
+    Close=float(sclaer.fit_transform(request.form.get('close')))	
+    Volume=float(scaler.fit_transform(request.form.get('volume')))
 
     prediction=model.predict('open,'high','low','close','volume')
 
