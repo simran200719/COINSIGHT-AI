00dc2ef3f63f45439a654dfe619d206054a8a278
gursimranjot kaur <simran19@users.noreply.huggingface.co>
2025-09-21 15:40:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,12 @@ scaler_x=joblib.load('scaler_x.pkl')
 
 @app.route('/')
 def home():
+    return render_template('home.html')
+    
+@app.roue('/start')
+def startt():
     return render_template('index.html')
-
+    
 @app.route('/predict',methods=['POST','GET'])
 def pred():
     Open=float(request.form.get('open'))
