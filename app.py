cd7ce237c3fcefa731ce25d57326980bac62a07a
gursimranjot kaur <simran19@users.noreply.huggingface.co>
2025-09-22 04:49:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,16 @@ scaler_x=joblib.load('scaler_x.pkl')
 @app.route('/')
 def home():
     try:
-        response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+        response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd', timeout=5)
+        response.raise_for_status()  # This will raise an HTTPError if status != 200
         data = response.json()
         btc_price = data['bitcoin']['usd']
     except Exception as e:
+        print("Error fetching price:", e)  # This prints error to console
         btc_price = "Error fetching price"
     
     return render_template('home.html', btc_price=btc_price)
+
 
     
 @app.route('/start')
